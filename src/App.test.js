02f24589f3351a748cb09f4119e84fcd48528169
roshniv <heyitsroshni@gmail.com>
2022-06-27 15:35:42 +1000

    Add files via upload

--- src/App.test.js
+++ src/App.test.js
@@ -1,39 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import { shallow } from "enzyme";
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-function renderApp(args) {
-  const defaultProps = {
-    city: '',
-    data: []
-  };
-
-  const props = { ...defaultProps, ...args };
-  return shallow(<App {...props} />);
-}
-
-it("renders search-bar on the initial load", () => {
-  const wrapper = renderApp();
-  // console.log(wrapper.debug());
-  expect(wrapper.find("#search-bar").length).toBe(1);
-});
-
-/*test('renders search-bar on the initial load', () => {
+test('renders search-bar on the initial load', () => {
   const { container } = render(<App />);
   const inputElement = container.querySelector('#search-bar');
   expect(inputElement).toBeInTheDocument();
 });
 
-test('enter key should trigger search call', () => {
-
-});
-
-test ('valid city should render weather data correctly', () => {
+test('every time value change happens to search-bar onChange gets called', () => {
   const { container } = render(<App />);
-  container.setData({main: {temp: 11, humidity: 52}});
-  const weatherCard = container.querySelector('.main');
+  const inputElement = container.querySelector('#search-bar');
+  const handleClick = jest.fn()
+  inputElement.addEventListener("change", handleClick);
+  fireEvent.change(inputElement, {target: {value: 'sydney'}});
+  expect(handleClick).toHaveBeenCalledTimes(1);
+  fireEvent.change(inputElement, {target: {value: 'sydney123'}});
+  expect(handleClick).toHaveBeenCalledTimes(2);
 });
 
-test('invalid city should render error card', () => {
-
-});*/
